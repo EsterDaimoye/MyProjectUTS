45f2bf36aed86879b8e031a1f1d6484ebf510329
ester <daimoyee@gmail.com>
2024-04-23 19:53:54 +0700

    have fun revisi1 Firebase Authentication

--- app/src/main/java/com/example/myprojectuts/Register.kt
+++ app/src/main/java/com/example/myprojectuts/Register.kt
@@ -3,25 +3,55 @@ package com.example.myprojectuts
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import com.example.myprojectuts.databinding.ActivityLoginBinding
 import com.example.myprojectuts.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
-class Register: AppCompatActivity() {
+class RegisterActivity: AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding= ActivityRegisterBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         binding.btnRegister.setOnClickListener{
-            val intent = Intent(this,Login::class.java)
-            startActivity(intent)
+            val email : String = binding.editTextTextEmailAddress2.text.toString().trim()
+            val password : String = binding.editTextPassword.text.toString().trim()
+            val configpassword : String = binding.editTextConfigPassword.text.toString().trim()
+
+            if (email.isEmpty()){
+                binding.editTextTextEmailAddress2.error = "Input Email"
+                binding.editTextTextEmailAddress2.requestFocus()
+                return@setOnClickListener
+            }
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.editTextTextEmailAddress2.error = "Invalid Email"
+                binding.editTextTextEmailAddress2.requestFocus()
+                return@setOnClickListener
+            }
+            if (password.isEmpty() || password.length< 6){
+                binding.editTextPassword.error = "Paasword must be more than 6 characters"
+                binding.editTextPassword.requestFocus()
+                return@setOnClickListener
+            }
+            if (password != configpassword){
+                binding.editTextConfigPassword.error = "password must be match"
+                binding.editTextConfigPassword.requestFocus()
+                return@setOnClickListener
+            }
         }
 
         binding.tvLogin.setOnClickListener{
-            val intent = Intent(this,Login::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, Login::class.java))
+
 
         }
     }
 }
+
